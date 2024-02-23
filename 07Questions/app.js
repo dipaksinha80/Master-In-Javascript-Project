19a0a69d0e6c45d606b38b47a9a9f34202214ea5
Dipak Sinha <dipaksinha2022@gmail.com>
2024-02-23 13:43:46 +0545

    again simple show hide question are made by using selector inside the elements

--- 07Questions/app.js
+++ 07Questions/app.js
@@ -1,13 +1,25 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  //   console.log(question);
+  const btn = question.querySelector(".question-btn");
+  //   console.log(btn);
+  btn.addEventListener("click", function () {
+    question.classList.toggle("show-text");
+  });
+});
+
 // traversing the dom
 
 // question-btn
 
-const queBtn = document.querySelectorAll(".question-btn");
+// const queBtn = document.querySelectorAll(".question-btn");
 
-queBtn.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const question = e.currentTarget.parentElement.parentElement; //this is targeting the  parent class
-    question.classList.toggle("show-text");
-  });
-});
+// queBtn.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement; //this is targeting the  parent class
+//     question.classList.toggle("show-text");
+//   });
+// });
