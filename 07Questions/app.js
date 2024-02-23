32b947aeb4c5b86d9d9d663ec08ff6176175bc9b
Dipak Sinha <dipaksinha2022@gmail.com>
2024-02-23 12:38:59 +0545

    simple show hide toggle is ready

--- 07Questions/app.js
+++ 07Questions/app.js
@@ -1,2 +1,13 @@
 //using selectors inside the element
 // traversing the dom
+
+// question-btn
+
+const queBtn = document.querySelectorAll(".question-btn");
+
+queBtn.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const question = e.currentTarget.parentElement.parentElement; //this is targeting the  parent class
+    question.classList.toggle("show-text");
+  });
+});
